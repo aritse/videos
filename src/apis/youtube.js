8fe54a585fa9efd5f136a2490f3fd18f76246662
Ari Tserendavaa <tserenda@hotmail.com>
2021-03-02 20:17:19 -0800

    add api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,2 +1,12 @@
+import axios from 'axios';
+
 const KEY = "AIzaSyBEKLEuNOe3N4QFlj6zKkwde9rih6yKCKQ";
 
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
